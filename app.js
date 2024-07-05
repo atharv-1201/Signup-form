6bf3555a0ec97ecfbe34fa369b6bd3b75e5c1202
Admin <ad5066@srmist.edu.in>
2024-07-05 14:26:56 +0530

    Edited the projectfolder name

---
+++ app.js
@@ -0,0 +1,10 @@
+// SIGNUP FORM/
+// ├── app.js
+// ├── routes.js
+// ├── confirm.js
+// ├── service.js
+// ├── config.js
+// ├── public/
+// │   ├── index.html
+// │   ├── styles.css
+// │   └── script.js
